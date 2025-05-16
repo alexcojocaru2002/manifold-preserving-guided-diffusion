7d2ab6f0fcf20317d58fd3fb13d7085900f9a585
Dimitris Xynogalas <dimitrisxynogalas@gmail.com>
2025-05-16 20:59:40 +0200

    Great start

--- main.py
+++ main.py
@@ -1,27 +1,365 @@
-import argparse
+from transformers import CLIPTextModel, CLIPTokenizer
+from diffusers import AutoencoderKL, UNet2DConditionModel, PNDMScheduler, DDIMScheduler
+import torch
+import os
+from PIL import Image
+from tqdm.auto import tqdm
+from typing import Optional, Tuple, Union
+from diffusers.schedulers.scheduling_ddim import DDIMSchedulerOutput
+from diffusers.schedulers.scheduling_ddim import randn_tensor
 
-from scripts import visualize_data
-import configparser
 
-if __name__ == '__main__':
-    import huggingface_hub
-    print(dir(huggingface_hub))
+class MPGDScheduler(DDIMScheduler):
+    def step(
+        self,
+        model_output: torch.Tensor,
+        timestep: int,
+        sample: torch.Tensor,
+        loss,
+        eta: float = 0.0,
+        learning_rate: float = 0.1,
+        use_clipped_model_output: bool = False,
+        generator=None,
+        variance_noise: Optional[torch.Tensor] = None,
+        return_dict: bool = True,
+    ) -> Union[DDIMSchedulerOutput, Tuple]:
+        """
+        Predict the sample from the previous timestep by reversing the SDE. This function propagates the diffusion
+        process from the learned model outputs (most often the predicted noise).
 
-    parser = argparse.ArgumentParser(
-        description="Run different scripts based on the provided arguments"
+        Args:
+            model_output (`torch.Tensor`):
+                The direct output from learned diffusion model.
+            timestep (`float`):
+                The current discrete timestep in the diffusion chain.
+            sample (`torch.Tensor`):
+                A current instance of a sample created by the diffusion process.
+            eta (`float`):
+                The weight of noise for added noise in diffusion step.
+            use_clipped_model_output (`bool`, defaults to `False`):
+                If `True`, computes "corrected" `model_output` from the clipped predicted original sample. Necessary
+                because predicted original sample is clipped to [-1, 1] when `self.config.clip_sample` is `True`. If no
+                clipping has happened, "corrected" `model_output` would coincide with the one provided as input and
+                `use_clipped_model_output` has no effect.
+            generator (`torch.Generator`, *optional*):
+                A random number generator.
+            variance_noise (`torch.Tensor`):
+                Alternative to generating noise with `generator` by directly providing the noise for the variance
+                itself. Useful for methods such as [`CycleDiffusion`].
+            return_dict (`bool`, *optional*, defaults to `True`):
+                Whether or not to return a [`~schedulers.scheduling_ddim.DDIMSchedulerOutput`] or `tuple`.
+
+        Returns:
+            [`~schedulers.scheduling_ddim.DDIMSchedulerOutput`] or `tuple`:
+                If return_dict is `True`, [`~schedulers.scheduling_ddim.DDIMSchedulerOutput`] is returned, otherwise a
+                tuple is returned where the first element is the sample tensor.
+
+        """
+        if self.num_inference_steps is None:
+            raise ValueError(
+                "Number of inference steps is 'None', you need to run 'set_timesteps' after creating the scheduler"
+            )
+
+        # See formulas (12) and (16) of DDIM paper https://arxiv.org/pdf/2010.02502.pdf
+        # Ideally, read DDIM paper in-detail understanding
+
+        # Notation (<variable name> -> <name in paper>
+        # - pred_noise_t -> e_theta(x_t, t)
+        # - pred_original_sample -> f_theta(x_t, t) or x_0
+        # - std_dev_t -> sigma_t
+        # - eta -> η
+        # - pred_sample_direction -> "direction pointing to x_t"
+        # - pred_prev_sample -> "x_t-1"
+
+        # 1. get previous step value (=t-1)
+        prev_timestep = (
+            timestep - self.config.num_train_timesteps // self.num_inference_steps
+        )
+
+        # 2. compute alphas, betas
+        alpha_prod_t = self.alphas_cumprod[timestep]
+        alpha_prod_t_prev = (
+            self.alphas_cumprod[prev_timestep]
+            if prev_timestep >= 0
+            else self.final_alpha_cumprod
+        )
+
+        beta_prod_t = 1 - alpha_prod_t
+
+        # 3. compute predicted original sample from predicted noise also called
+        # "predicted x_0" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        pred_original_latent_sample = (
+            (1 / alpha_prod_t ** (0.5)) * (sample - beta_prod_t ** (0.5) * model_output)
+        ).requires_grad_(True)
+
+        pred_original_latent_sample = (
+            pred_original_latent_sample
+            - learning_rate
+            * torch.autograd.grad(
+                loss(pred_original_latent_sample),
+                pred_original_latent_sample,
+                retain_graph=True,
+                create_graph=True,
+                allow_unused=True,
+            )[0]
+        )
+
+        pred_original_sample = pred_original_latent_sample
+        pred_epsilon = model_output
+
+        if self.config.prediction_type == "epsilon":
+            pred_original_sample = (
+                sample - beta_prod_t ** (0.5) * model_output
+            ) / alpha_prod_t ** (0.5)
+            pred_epsilon = model_output
+        elif self.config.prediction_type == "sample":
+            pred_original_sample = model_output
+            pred_epsilon = (
+                sample - alpha_prod_t ** (0.5) * pred_original_sample
+            ) / beta_prod_t ** (0.5)
+        elif self.config.prediction_type == "v_prediction":
+            pred_original_sample = (alpha_prod_t**0.5) * sample - (
+                beta_prod_t**0.5
+            ) * model_output
+            pred_epsilon = (alpha_prod_t**0.5) * model_output + (
+                beta_prod_t**0.5
+            ) * sample
+        else:
+            raise ValueError(
+                f"prediction_type given as {self.config.prediction_type} must be one of `epsilon`, `sample`, or"
+                " `v_prediction`"
+            )
+
+        # 4. Clip or threshold "predicted x_0"
+        if self.config.thresholding:
+            pred_original_sample = self._threshold_sample(pred_original_sample)
+        elif self.config.clip_sample:
+            pred_original_sample = pred_original_sample.clamp(
+                -self.config.clip_sample_range, self.config.clip_sample_range
+            )
+
+        # 5. compute variance: "sigma_t(η)" -> see formula (16)
+        # σ_t = sqrt((1 − α_t−1)/(1 − α_t)) * sqrt(1 − α_t/α_t−1)
+        variance = self._get_variance(timestep, prev_timestep)
+        std_dev_t = eta * variance ** (0.5)
+
+        if use_clipped_model_output:
+            # the pred_epsilon is always re-derived from the clipped x_0 in Glide
+            pred_epsilon = (
+                sample - alpha_prod_t ** (0.5) * pred_original_sample
+            ) / beta_prod_t ** (0.5)
+
+        # 6. compute "direction pointing to x_t" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        # This is line 7 from MPGD
+        pred_sample_direction = (1 - alpha_prod_t_prev - std_dev_t**2) ** (
+            0.5
+        ) * pred_epsilon
+
+        # 7. compute x_t without "random noise" of formula (12) from https://arxiv.org/pdf/2010.02502.pdf
+        # This is line 6 from MPGD
+        prev_sample = (
+            alpha_prod_t_prev ** (0.5) * pred_original_sample + pred_sample_direction
+        )
+
+        if eta > 0:
+            if variance_noise is not None and generator is not None:
+                raise ValueError(
+                    "Cannot pass both generator and variance_noise. Please make sure that either `generator` or"
+                    " `variance_noise` stays `None`."
+                )
+
+            if variance_noise is None:
+                variance_noise = randn_tensor(
+                    model_output.shape,
+                    generator=generator,
+                    device=model_output.device,
+                    dtype=model_output.dtype,
+                )
+            variance = std_dev_t * variance_noise
+
+            prev_sample = prev_sample + variance
+
+        if not return_dict:
+            return (
+                prev_sample,
+                pred_original_sample,
+            )
+
+        return DDIMSchedulerOutput(
+            prev_sample=prev_sample, pred_original_sample=pred_original_sample
+        )
+
+
+# 1. Load the autoencoder model which will be used to decode the latents into image space.
+vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
+
+# 2. Load the tokenizer and text encoder to tokenize and encode the text.
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
+
+# 3. The UNet model for generating the latents.
+unet = UNet2DConditionModel.from_pretrained(
+    "CompVis/stable-diffusion-v1-4", subfolder="unet"
+)
+
+
+from diffusers import DDIMScheduler, LMSDiscreteScheduler
+
+# scheduler = LMSDiscreteScheduler(
+#     beta_start=0.00085,
+#     beta_end=0.012,
+#     beta_schedule="scaled_linear",
+#     num_train_timesteps=1000,
+# )
+scheduler = DDIMScheduler.from_pretrained(
+    "CompVis/stable-diffusion-v1-4", subfolder="scheduler", eta=0.0
+)
+
+torch_device = "cuda"
+vae.to(torch_device)
+text_encoder.to(torch_device)
+unet.to(torch_device)
+
+prompt = ["a photograph of an astronaut riding a horse"]
+
+height = 512  # default height of Stable Diffusion
+width = 512  # default width of Stable Diffusion
+
+num_inference_steps = 100  # Number of denoising steps
+
+guidance_scale = 7.5  # Scale for classifier-free guidance
+
+generator = torch.manual_seed(42)  # Seed generator to create the initial latent noise
+
+batch_size = len(prompt)
+
+text_input = tokenizer(
+    prompt,
+    padding="max_length",
+    max_length=tokenizer.model_max_length,
+    truncation=True,
+    return_tensors="pt",
+)
+
+text_embeddings = text_encoder(text_input.input_ids.to(torch_device))[0]
+
+max_length = text_input.input_ids.shape[-1]
+uncond_input = tokenizer(
+    [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
+)
+
+# uncond_input = tokenizer([""] * batch_size, return_tensors="pt")
+uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
+
+text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+# text_embeddings = uncond_embeddings
+
+latents = torch.randn(
+    (batch_size, unet.in_channels, height // 8, width // 8),
+    generator=generator,
+)
+latents = latents.to(torch_device)
+
+scheduler.set_timesteps(num_inference_steps)
+
+latents = latents * scheduler.init_noise_sigma
+
+for t in tqdm(scheduler.timesteps):
+    # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+    latent_model_input = torch.cat([latents] * 2)
+    # latent_model_input = latents
+
+    latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
+
+    # predict the noise residual
+    with torch.no_grad():
+        noise_pred = unet(
+            latent_model_input, t, encoder_hidden_states=text_embeddings
+        ).sample
+
+    # perform guidance
+    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+    noise_pred = noise_pred_uncond + guidance_scale * (
+        noise_pred_text - noise_pred_uncond
     )
-    subparsers = parser.add_subparsers(dest="script_name", required=True,
-                                       help="Script to run")
 
-    parser_visualize_data = subparsers.add_parser("visualize_data", help="Visualize data")
-    parser_visualize_data.add_argument(
-        '--num_samples', type=int, required=True, help='Number of samples to visualize'
-    )
-    parser_visualize_data.add_argument(
-        '--prompt', type=str, default="a cat",
-        help='Text prompt for image generation'
-    )
+    # compute the previous noisy sample x_t -> x_t-1
+    latents = scheduler.step(noise_pred, t, latents).prev_sample
 
-    args = parser.parse_args()
-    if args.script_name == "visualize_data":
-        visualize_data(num_samples=args.num_samples, prompt=args.prompt)
+
+# scale and decode the image latents with vae
+scaling_factor = getattr(vae.config, "scaling_factor", 0.18215)
+latents = latents / scaling_factor
+with torch.no_grad():
+    image = vae.decode(latents).sample
+
+y = image
+
+image = (image / 2 + 0.5).clamp(0, 1)
+image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+images = (image * 255).round().astype("uint8")
+pil_images = [Image.fromarray(image) for image in images]
+pil_images[0].save("reference.png")
+
+#################################################################################################
+
+scheduler = MPGDScheduler.from_pretrained(
+    "CompVis/stable-diffusion-v1-4", subfolder="scheduler", eta=0.0
+)
+scheduler.set_timesteps(num_inference_steps)
+
+
+def loss(y):
+    def _loss(clean_image_latent_estimation):
+        # scale and decode the image latents with vae
+        scaling_factor = getattr(vae.config, "scaling_factor", 0.18215)
+        latents = clean_image_latent_estimation / scaling_factor
+        image = vae.decode(latents).sample
+
+        return torch.mean((image - y) ** 2)
+
+    return _loss
+
+
+uncond_input = tokenizer([""] * batch_size, return_tensors="pt")
+uncond_embeddings = text_encoder(uncond_input.input_ids.to(torch_device))[0]
+text_embeddings = uncond_embeddings
+
+latents = torch.randn(
+    (batch_size, unet.in_channels, height // 8, width // 8),
+    generator=generator,
+)
+latents = latents.to(torch_device)
+
+scheduler.set_timesteps(num_inference_steps)
+
+latents = latents * scheduler.init_noise_sigma
+
+for t in tqdm(scheduler.timesteps):
+    # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+    latent_model_input = latents
+
+    latent_model_input = scheduler.scale_model_input(latent_model_input, timestep=t)
+
+    # predict the noise residual
+    with torch.no_grad():
+        noise_pred = unet(
+            latent_model_input, t, encoder_hidden_states=text_embeddings
+        ).sample
+
+    # compute the previous noisy sample x_t -> x_t-1
+    latents = scheduler.step(noise_pred, t, latents, loss=loss(y)).prev_sample
+
+
+# scale and decode the image latents with vae
+scaling_factor = getattr(vae.config, "scaling_factor", 0.18215)
+latents = latents / scaling_factor
+with torch.no_grad():
+    image = vae.decode(latents).sample
+
+image = (image / 2 + 0.5).clamp(0, 1)
+image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+images = (image * 255).round().astype("uint8")
+pil_images = [Image.fromarray(image) for image in images]
+pil_images[0].save(f"result.png")
+# pil_images[0].save(os.path.join("visualization", f"test.png"))
