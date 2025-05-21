02cf3cfe153ee8ca5f03b62449383aee9e1d5244
Alexandru Cojocaru <alex.cojocaru.2002@gmail.com>
2025-05-21 13:07:19 +0200

    Added manual inference loop in ourmodel.py

--- model/OurModel.py
+++ model/OurModel.py
@@ -1,14 +1,92 @@
 import torch
-from diffusers import StableDiffusionPipeline
+from PIL import Image
+from diffusers import StableDiffusionPipeline, AutoencoderKL, UNet2DConditionModel, LMSDiscreteScheduler
+from tqdm.auto import tqdm
+from transformers import CLIPTokenizer, CLIPTextModel
+
 
 class OurModel():
     def __init__(self):
         super().__init__()
-        self.pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
-        print(torch.cuda.is_available())
-        if torch.cuda.is_available():
-            self.pipe.to("cuda")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def __call__(self, prompt):
-        image = self.pipe(prompt).images[0]
-        return image
+        # self.pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float16)
+        # 1. Load the autoencoder model which will be used to decode the latents into image space.
+        self.vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae").to(self.device)
+
+        # 2. Load the tokenizer and text encoder to tokenize and encode the text.
+        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
+        self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14").to(self.device)
+        self.scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+
+        # 3. The UNet model for generating the latents.
+        self.unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet").to(self.device)
+
+        # Image specific setup
+        self.height = 512  # default height of Stable Diffusion
+        self.width = 512  # default width of Stable Diffusion
+        self.num_inference_steps = 100  # Number of denoising steps
+        self.guidance_scale = 7.5  # Scale for classifier-free guidance
+        self.generator = torch.manual_seed(32)  # Seed generator to create the initial latent noise
+
+    def denoising_loop(self, prompt, num_inference_steps):
+
+        # TO DO: FIGURE OUT HOW TO set this arbitrarily, bigger number here causes error
+        batch_size = len([1])
+        text_input = self.tokenizer(prompt, padding="max_length", max_length=self.tokenizer.model_max_length,
+                                    truncation=True,
+                                    return_tensors="pt")
+
+        with torch.no_grad():
+            text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
+
+        max_length = text_input.input_ids.shape[-1]
+        uncond_input = self.tokenizer(
+            [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
+        )
+        with torch.no_grad():
+            uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
+
+        text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
+
+        latents = torch.randn(
+            (batch_size, self.unet.config.in_channels, self.height // 8, self.width // 8),
+            generator=self.generator,
+        ).to(self.device)
+
+        print(latents.shape)
+
+        self.scheduler.set_timesteps(num_inference_steps)
+        latents = latents * self.scheduler.init_noise_sigma
+
+        for t in tqdm(self.scheduler.timesteps):
+            # expand the latents if we are doing classifier-free guidance to avoid doing two forward passes.
+            latent_model_input = torch.cat([latents] * 2)
+
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, timestep=t)
+
+            # predict the noise residual
+            with torch.no_grad():
+                # print("latent_model_input.shape =", latent_model_input.shape)
+                noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+
+            # perform guidance
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond + self.guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+
+        return latents
+
+    def __call__(self, prompt, num_inference_steps):
+        latents = self.denoising_loop(prompt, num_inference_steps)
+        latents = 1 / 0.18215 * latents
+        with torch.no_grad():
+            image = self.vae.decode(latents).sample
+
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
+        images = (image * 255).round().astype("uint8")
+        pil_images = [Image.fromarray(image) for image in images]
+        return pil_images[0]
--- scripts/visualize_data.py
+++ scripts/visualize_data.py
@@ -8,8 +8,9 @@ from model import OurModel
 def visualize_data(model, prompt, num_samples):
     print("Showing image")
     for i in range(num_samples):
-        image = model(prompt)
+        image = model(prompt, 50)
         image.save("data/image_" + str(i) + ".png")
+        print("Image saved in " + "data/image_" + str(i) + ".png")
 
 def run(num_samples=1, prompt="a cat", output_dir='visualizations' ):
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
